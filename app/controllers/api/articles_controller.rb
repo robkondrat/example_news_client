26ddb865a72182624a33a3e17eafd99ea7421a66
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-02-04 13:34:09 -0600

    in class exercise

---
+++ app/controllers/api/articles_controller.rb
@@ -0,0 +1,9 @@
+class Api::ArticlesController < ApplicationController
+  def index
+    search_term = params[:search]
+    response = HTTP.headers("X-Api-Key" => Rails.application.credentials.news_api[:api_key]).get("https://newsapi.org/v2/everything?q=#{ search_term }")
+
+    @articles = response.parse
+    render "index.json.jb"
+  end
+end
